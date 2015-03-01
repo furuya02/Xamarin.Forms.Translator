0fc11d5e757aef03713959946eb2a5620eaacaea
SIN <sin@sapporoworks.ne.jp>
2015-03-02 04:26:30 +0900

    2015.03.02 0440

--- Translator/Translator/Translator.cs
+++ Translator/Translator/Translator.cs
@@ -10,66 +10,13 @@ using Newtonsoft.Json;
 namespace Translator {
     //PM> Install-Package Microsoft.Net.Http
     //PM> Install-Package Newtonsoft.Json
-    
-    class Translator {
 
-        private const string ClientId = "ClientId";
-        private const string ClientSecret = "ClientSecret";
-
-        private string _token;
-
-        private async Task<bool> InitializeToken() {
-            if (_token == null) {
-                var client = new HttpClient();
-                const string url = "https://datamarket.accesscontrol.windows.net/v2/OAuth2-13";
-                var content = new FormUrlEncodedContent(new Dictionary<string, string> {
-                    {"client_id", ClientId},
-                    {"client_secret", ClientSecret},
-                    {"scope", "http://api.microsofttranslator.com"},
-                    {"grant_type", "client_credentials"},
-                });
-                var response = await client.PostAsync(url, content);
-                //JSON形式のレスポンスからaccess_toneを取得する
-                var adm = JsonConvert.DeserializeObject<AdmAccessToken>(await response.Content.ReadAsStringAsync());
-                _token = adm.access_token;
-                return true;
-            }
-            return false;
-        }
-
-    
-        //JSON形式のレスポンスをデシリアライズするためのクラス
-        [DataContract]
-        public class AdmAccessToken {
-            [DataMember]
-            public string access_token { get; set; }
-
-            //access_token以外は、取得しないので省略
-        }
-
-        public async Task<string> Get(string str) {
-
-            //初回のみaccess_tokenを取得する
-            if (_token == null) {
-                await InitializeToken();
-            }
-            var client = new HttpClient();
-            var url = string.Format("http://api.microsofttranslator.com/v2/Http.svc/Translate?text={0}&to=ja"
-                , WebUtility.UrlEncode(str));
-            client.DefaultRequestHeaders.Add("Authorization", "Bearer " + _token);
-            var response = await client.GetStringAsync(url);
-            //XML解釈
-            var doc = XDocument.Parse(response);
-            return doc.Root.FirstNode.ToString();
-        }
-    }
-
-    /*internal class Translator {
+    internal class Translator {
         //登録されたアプリの一覧
         //https://datamarket.azure.com/developer/applications
 
-        private const string ClientId = "TranslatorSample001";
-        private const string ClientSecret = "iYbcPHUz8TZ7rYdoL9wyGc3PXT8QYsiCCE8zFfq7IVE=";
+        private const string ClientId = "ClientId";
+        private const string ClientSecret = "ClientSecret";
 
         private string _token;
 
@@ -136,5 +83,5 @@ namespace Translator {
             }
             return null;
         }
-    }*/
+    }
 }
